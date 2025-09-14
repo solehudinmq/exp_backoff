875a06487be56caa49082c2cd0dde966cfe51411
SolehMQ <solehudinmq@gmail.com>
2025-09-15 06:01:18 +0700

    first commit

---
+++ lib/exp_backoff.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "exp_backoff/version"
+
+module ExpBackoff
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/exp_backoff/version.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+module ExpBackoff
+  VERSION = "0.1.0"
+end
